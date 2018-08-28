602b10e9f92e06613523ce9793b6c8fedd25fb20
Farid Arjmand <faridarjmand73@gmail.com>
2018-08-28 13:10:01 +0430

    Update REDIS.sh

--- REDIS.sh
+++ REDIS.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+## Created By.Farid Arjmand ##
+
+###############################
+########## Functions ##########
+###############################
+
 JSON ()
 {
         echo -e "{\n\t\"data\":[\n"
@@ -22,7 +28,11 @@ JSON ()
         done
         echo -e "\n\t { \"{#END}\":\"END\" }\n\n\t]\n}"
 }
-#########################################################
+
+##############################
+############ Main ############
+##############################
+
 case $1 in
         ping)if [ `$2/redis-cli -h $3 -p $4 ping` == "PONG" ];then echo "UP"; else echo "DOWN"; fi 2>/dev/null;;
         reload)JSON > $HOME/REDIS.tmp 2>/dev/null && echo OK;;
@@ -32,37 +42,6 @@ case $1 in
         *)cat $HOME/REDIS.tmp
 esac
 
-#!/bin/bash
-HOME="/export/home/zabbix"
-JSON ()
-{
-        echo -e "{\n\t\"data\":[\n"
-        for PID in `ps -ef | pgrep "redis"`;do
-                ADDRESS=$(lsof -p $PID | awk '/cwd/{print $NF}')
-                PORT=$(echo info | $ADDRESS/redis-cli | awk -F":" '/tcp_port/{print $2}' | dos2unix)
-                NAME=$(echo $ADDRESS | awk -F"/" '{print $NF}')
-                if [ -z $PORT ];then
-                        PORT=$(lsof -p $PID | awk '/LISTEN/{print $(NF-1)}' | awk -F":" '{print $2}')
-                fi
-                for j in `/usr/sbin/ifconfig -a | awk '/inet/{print $2}'`;do
-                        if [ `$ADDRESS/redis-cli -h $j -p $PORT ping` == "PONG" ];then
-                                IP=$j
-                        fi
-                done
-                echo -e "\t {\n\t  \"{#REDIS}\":\"$ADDRESS\","
-                echo -e "\t  \"{#NAME}\":\"$NAME\","
-                echo -e "\t  \"{#PORT}\":\"$PORT\","
-                echo -e "\t  \"{#PID}\":\"$PID\","
-                echo -e "\t  \"{#IP}\":\"$IP\" \n\t },"
-        done
-        echo -e "\n\t { \"{#END}\":\"END\" }\n\n\t]\n}"
-}
-#########################################################
-case $1 in
-        ping)if [ `$2/redis-cli -h $3 -p $4 ping` == "PONG" ];then echo "UP"; else echo "DOWN"; fi 2>/dev/null;;
-        reload)JSON > $HOME/REDIS.tmp 2>/dev/null && echo OK;;
-        db)$2/redis-cli -h $3 -p $4 info | grep -c ^db 2>/dev/null;;
-        size)$2/redis-cli -h $3 -p $4 dbsize | awk '{print $NF}';;
-        status)$2/redis-cli -h $3 -p $4 info | /usr/bin/gawk -F":" -v SEARCH="$5" 'match($0, SEARCH) {print $2}';;
-        *)cat $HOME/REDIS.tmp
-esac
+##############################
+############ END #############
+##############################
